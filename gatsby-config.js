fa7557b7fdb1f9dbe801ee8032a34125d70eabfa
Eric Grossman <eric.josh.grossman@gmail.com>
2020-01-06 23:45:05 -0600

    banner now?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,47 @@
 require(`dotenv`).config({
- path: `.env`,
+  path: `.env`,
 })
 
 module.exports = {
- siteMetadata: {
-  siteTitleAlt: `Portfolio`,
- },
- plugins: [
-  {
-   resolve: `@lekoarts/gatsby-theme-cara`,
-   options: {},
+  siteMetadata: {
+    siteTitleAlt: `Portfolio`,
   },
-  {
-   resolve: `gatsby-plugin-google-analytics`,
-   options: {
-    trackingId: process.env.GOOGLE_ANALYTICS_ID,
-   },
-  },
-  {
-   resolve: `gatsby-plugin-manifest`,
-   options: {
-    name: `Eric Grossman`,
-    short_name: `Eric Grossman`,
-    description: `Playful and Colorful One-Page portfolio`,
-    start_url: `/`,
-    background_color: `#141821`,
-    theme_color: `#f6ad55`,
-    display: `standalone`,
-    icons: [
-     {
-      src: `/android-chrome-192x192.png`,
-      sizes: `192x192`,
-      type: `image/png`,
-     },
-     {
-      src: `/android-chrome-512x512.png`,
-      sizes: `512x512`,
-      type: `image/png`,
-     },
-    ],
-   },
-  },
-  `gatsby-plugin-offline`,
-  `gatsby-plugin-netlify`,
- ],
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Eric Grossman`,
+        short_name: `Eric Grossman`,
+        description: `Playful and Colorful One-Page portfolio`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/banner.jpg`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/banner.jpg`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
 }
